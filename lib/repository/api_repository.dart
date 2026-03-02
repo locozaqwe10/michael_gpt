7b1cb5e8b31474ee9d3139bc396053dbf2cefbde
locozaqwe10~ <JoaoVictorCarvalhoSilva0126@outlook.com>
2026-03-03 01:08:12 +0200

    MVP Final

---
+++ lib/repository/api_repository.dart
@@ -0,0 +1,326 @@
+import 'dart:convert';
+
+import 'package:custom_chat_gpt/data/api_urls.dart';
+import 'package:custom_chat_gpt/model/general_response.dart';
+import 'package:custom_chat_gpt/model/payment_model.dart';
+import 'package:http/http.dart';
+
+import '../data/network/Network_api_service.dart';
+import '../data/network/base_api_services.dart';
+
+class ApiRepository {
+  final BaseApiService _apiService = NetworkApiService();
+
+  Future<dynamic> postCreateUser(dynamic datamap) async {
+    Response response;
+    try {
+      response = await _apiService.getPostApiResponse(
+        ApiUrls.CREATE_USER,
+        datamap,
+      );
+
+      var jsonResponse = jsonDecode(response.body.toString());
+
+      if (response.statusCode == 200) {
+        return GeneralResponse(
+          message: jsonResponse["message"],
+          user: "",
+          code: response.statusCode,
+          data: jsonResponse["data"],
+        );
+        response.body.toString();
+      } else {
+        return GeneralResponse(
+          message: jsonResponse["detail"],
+          user: "",
+          code: response.statusCode,
+          data: {},
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+      return GeneralResponse(
+        message: e.toString(),
+        user: "",
+        code: 500,
+        data: {},
+      );
+
+      rethrow;
+    }
+  }
+
+  Future<GeneralResponse> postUserLogin(dynamic datamap) async {
+    Response response;
+    try {
+      response = await _apiService.getPostApiResponse(
+        ApiUrls.LOGIN_USER,
+        datamap,
+      );
+
+      var jsonResponse = jsonDecode(response.body.toString());
+
+      if (response.statusCode == 200) {
+        return GeneralResponse(
+          message: jsonResponse["message"],
+          user: "",
+          code: response.statusCode,
+          data: jsonResponse["data"],
+        );
+        response.body.toString();
+      } else {
+        return GeneralResponse(
+          message: jsonResponse["detail"],
+          user: "",
+          code: response.statusCode,
+          data: {},
+        );
+      }
+    } catch (e) {
+      print(e.toString()+"  Repo 0x00001");
+      return GeneralResponse(
+        message: e.toString(),
+        user: "",
+        code: 500,
+        data: {},
+      );
+
+      rethrow;
+    }
+  }
+
+  Future<GeneralResponse> postAddLLMmessage(dynamic datamap) async {
+    Response response;
+    try {
+      response = await _apiService.getPostApiResponse(
+        ApiUrls.ADD_LLM_MESSAGE,
+        datamap,
+      );
+
+      var jsonResponse = jsonDecode(response.body.toString());
+
+      if (response.statusCode == 200) {
+        return GeneralResponse(
+          message: jsonResponse["message"],
+          user: "",
+          code: response.statusCode,
+          data: jsonResponse["data"],
+        );
+        response.body.toString();
+      } else {
+        return GeneralResponse(
+          message: jsonResponse["detail"],
+          user: "",
+          code: response.statusCode,
+          data: {},
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+      return GeneralResponse(
+        message: e.toString(),
+        user: "",
+        code: 500,
+        data: {},
+      );
+
+      rethrow;
+    }
+  }
+
+  Future<GeneralResponse> postAddSessionID(String userId) async {
+    Response response;
+    try {
+      response = await _apiService.getPostApiResponseWithoutParam(
+        ApiUrls.ADD_CHAT_SESSION+userId,
+
+      );
+
+      var jsonResponse = jsonDecode(response.body.toString());
+
+      if (response.statusCode == 200) {
+        return GeneralResponse(
+          message: jsonResponse["message"],
+          user: "",
+          code: response.statusCode,
+          data: jsonResponse["data"],
+        );
+        response.body.toString();
+      } else {
+        return GeneralResponse(
+          message: jsonResponse["detail"],
+          user: "",
+          code: response.statusCode,
+          data: {},
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+      return GeneralResponse(
+        message: e.toString(),
+        user: "",
+        code: 500,
+        data: {},
+      );
+
+      rethrow;
+    }
+  }
+
+
+  Future<GeneralResponse> getChatHisotry(String userId) async {
+    Response response;
+    try {
+      response = await _apiService.getPostApiResponseWithoutParam(
+        ApiUrls.CHAT_HISTORY+userId,
+
+      );
+
+      var jsonResponse = jsonDecode(response.body.toString());
+
+      if (response.statusCode == 200) {
+        return GeneralResponse(
+          message: jsonResponse["message"],
+          user: "",
+          code: response.statusCode,
+          data: jsonResponse["data"][0],
+        );
+        response.body.toString();
+      } else {
+        return GeneralResponse(
+          message: jsonResponse["detail"],
+          user: "",
+          code: response.statusCode,
+          data: {},
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+      return GeneralResponse(
+        message: e.toString(),
+        user: "",
+        code: 500,
+        data: {},
+      );
+
+      rethrow;
+    }
+  }
+
+  Future<GeneralResponse> getUserSubscription (String userId) async {
+    Response response;
+    try {
+      response = await _apiService.getGetResponse(
+        ApiUrls.USER_SUBSCRIPTION+userId,
+
+      );
+
+      var jsonResponse = jsonDecode(response.body.toString());
+
+      if (response.statusCode == 200) {
+        return GeneralResponse(
+          message: jsonResponse["message"],
+          user: "",
+          code: response.statusCode,
+          data: jsonResponse["data"][0],
+        );
+        response.body.toString();
+      } else {
+        return GeneralResponse(
+          message: jsonResponse["detail"],
+          user: "",
+          code: response.statusCode,
+          data: {},
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+      return GeneralResponse(
+        message: e.toString(),
+        user: "",
+        code: 500,
+        data: {},
+      );
+
+      rethrow;
+    }
+
+  }
+
+
+  Future<GeneralResponse> postUpgradeSubscription(PaymentModel payment) async {
+    Response response;
+    try {
+      response = await _apiService.getPostApiResponse(
+        ApiUrls.UPGRADE_SUBSCRIPTION,payment.toJson() as Map<String, String>
+
+      );
+
+      var jsonResponse = jsonDecode(response.body.toString());
+
+      if (response.statusCode == 200) {
+        return GeneralResponse(
+          message: jsonResponse["message"],
+          user: "",
+          code: response.statusCode,
+          data: jsonResponse["payment"],
+        );
+        response.body.toString();
+      } else {
+        return GeneralResponse(
+          message: jsonResponse["detail"],
+          user: "",
+          code: response.statusCode,
+          data: {},
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+      return GeneralResponse(
+        message: e.toString(),
+        user: "",
+        code: 500,
+        data: {},
+      );
+
+      rethrow;
+    }
+  }
+
+
+  Future<GeneralResponse> postgetKeyFromAPI(int amount) async {
+    Response response;
+    try {
+      response = await _apiService.getPostApiResponseWithoutParam(
+          ApiUrls.CREATE_PAYMENT_INTENT+ amount.toString());
+
+      var jsonResponse = jsonDecode(response.body.toString());
+
+      if (response.statusCode == 200) {
+        return GeneralResponse(
+          message: jsonResponse["message"],
+          user: "",
+          code: response.statusCode,
+          data: jsonResponse["data"],
+        );
+      } else {
+        return GeneralResponse(
+          message: jsonResponse["detail"],
+          user: "",
+          code: response.statusCode,
+          data: {},
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+      return GeneralResponse(
+        message: e.toString(),
+        user: "",
+        code: 500,
+        data: {},
+      );
+
+      rethrow;
+    }
+  }
+}
---
+++ lib/utilities/hive/user_hive_model.g.dart
@@ -0,0 +1,64 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_hive_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class UserHiveModelAdapter extends TypeAdapter<UserHiveModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  UserHiveModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return UserHiveModel(
+      userId: fields[0] as String,
+      email: fields[1] as String,
+      userName: fields[2] as String,
+      subscriptionId: fields[3] as int,
+      subscriptionInTokenAllowed: fields[4] as int,
+      subscriptionOutTokenAllowed: fields[5] as int,
+      userInToken: fields[6] as int,
+      userOutToken: fields[7] as int,
+      token: fields[8] as String? ?? '',
+      subscriptionFriendlyName: fields[9] as String? ?? ''
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, UserHiveModel obj) {
+    writer
+      ..writeByte(8)
+      ..writeByte(0)
+      ..write(obj.userId)
+      ..writeByte(1)
+      ..write(obj.email)
+      ..writeByte(2)
+      ..write(obj.userName)
+      ..writeByte(3)
+      ..write(obj.subscriptionId)
+      ..writeByte(4)
+      ..write(obj.subscriptionInTokenAllowed)
+      ..writeByte(5)
+      ..write(obj.subscriptionOutTokenAllowed)
+      ..writeByte(6)
+      ..write(obj.userInToken)
+      ..writeByte(7)
+      ..write(obj.userOutToken);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is UserHiveModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
